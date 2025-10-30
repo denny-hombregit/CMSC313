437907eb2d3894c75bac259b3326d96a3b842bd0
denny-hombregit <dennya1@umbc.edu>
2025-10-30 17:59:24 -0400

    Update intro.cpp
    Updated code to add "Hello, teach." to the sentence. Forgot about that part when I first made it. Now updated.

--- intro.cpp
+++ intro.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    cout << "My name is Denny Amaya and I'm in your CMSC313 class" << endl;
+    cout << "Hello, teach. My name is Denny Amaya and I'm in your CMSC313 class" << endl;
     // Changed the course name to reflect the truth 
     return 0;
 }
